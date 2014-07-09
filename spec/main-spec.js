93f405ba28d14d25e210f5e3288436bea572b739
Nuno Barreto <nbarr@YDSERVER-LX.(none)>
2014-07-09 15:35:17 +0000

    Basic skeleton

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,13 +1,13 @@
-var neo4j = require('neo4j');
-var db = new neo4j.GraphDatabase('http://localhost:7474');
-
+var neo4j = require('node-neo4j');
 var acl = require("../lib/acl.js");
-var neo4jConnector = require("../lib/connectors/neo4j.js");
 
 describe("addUserRoles", function () {
   it("should add roles to a user", function () {
-    var acl1 = new acl(new neo4jConnector(db));
+    var db = new neo4j('http://localhost:7474');
+    var acl1 = new acl(new acl.neo4jConnector(db));
+
     var addUserRole = acl1.addUserRoles('user', 'group');
+
     expect(addUserRole).toBe(true);
   });
 });
