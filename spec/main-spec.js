d9d3ebf1bd93c13a28a10c449d60c4ffd4dfc6d5
Nuno Barreto <nbarreto@gmail.com>
2014-07-11 16:47:10 +0000

    added testes for neo4j connector

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,13 +1,67 @@
 var neo4j = require('node-neo4j');
-var acl = require("../lib/acl.js");
+var acl = require("../index.js");
 
-describe("addUserRoles", function () {
-  it("should add roles to a user", function () {
-    var db = new neo4j('http://localhost:7474');
-    var acl1 = new acl(new acl.neo4jConnector(db));
+var db = new neo4j('http://localhost:7474');
+var testACL = new acl(new acl.neo4jConnector(db));
 
-    var addUserRole = acl1.addUserRoles('user', 'group');
+var properties1 = {
+  p1: 'prop 1',
+  p2: 'prop 2'
+};
+var labels1 = [
+  'Label1',
+  'Label2'
+];
+var groups1 = [
+  'group1',
+  'group2'
+];
 
-    expect(addUserRole).toBe(true);
+var properties2 = {
+  p3: 'prop 3',
+  p4: 'prop 4'
+};
+var labels2 = [
+  'Label3',
+  'Label4'
+];
+var groups2 = [
+  'group3',
+  'group4'
+];
+/*
+describe("Call addUserRoles with userID and a single role", function () {
+  var node;
+
+  beforeEach(function(done) {
+    testACL.addUserRoles('user1', ['group'], function(err, node1) {
+      node = node1;
+      done();
+    });
+  });
+
+  it("should create a user and a role", function (done) {
+    expect(node).toBeDefined();
+    done();
   });
 });
+
+describe("Call addUserRoles with userID and a single role", function () {
+  var node;
+
+  beforeEach(function(done) {
+    testACL.addUserRoles('user1', ['group'], function(err, node1) {
+      node = node1;
+      done();
+    });
+  });
+
+  it("should create a user and a role", function (done) {
+    expect(node).toBeDefined();
+    done();
+  });
+});*/
+
+
+
+// After all tests, delete nodes created for testing
